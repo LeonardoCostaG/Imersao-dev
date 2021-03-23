8cfabf32e845749ab06c56abe5ec2d62fe5f496f
LeonardoCostaG <leonardocg.lcg@gmail.com>
2021-03-23 20:23:29 -0300

    js correction

--- Desafios/Aula-02/script.js
+++ Desafios/Aula-02/script.js
@@ -5,5 +5,5 @@ if (pokemon == 'Dragonite') {
 } else if(pokemon == 'dragonite') {
     document.write("<h2>Você acertou!!</h2>")
 } else {
-    document.write("<h2>O Pokémon corretosss é um Dragonite!</h2>")
+    document.write("<h2>O Pokémon correto é um Dragonite!</h2>")
   }
