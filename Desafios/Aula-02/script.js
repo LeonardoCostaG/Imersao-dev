04d8c9671da89f9f085eae2e917fc44a58082131
LeonardoCostaG <leonardocg.lcg@gmail.com>
2021-03-23 20:08:41 -0300

    class 02 challange

---
+++ Desafios/Aula-02/script.js
@@ -0,0 +1,9 @@
+var pokemon = prompt('Qual é o famoso Pokémon do treinador Lance (elite-four)?')
+
+if (pokemon == 'Dragonite') {
+  document.write("<h2>Você acertou!!</h2>")
+} else if(pokemon == 'dragonite') {
+    document.write("<h2>Você acertou!!</h2>")
+} else {
+    document.write("<h2>O Pokémon corretosss é um Dragonite!</h2>")
+  }
