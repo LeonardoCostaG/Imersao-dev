0d9997034227708b25d26fa0951c608e442a48a1
LeonardoCostaG <leonardocg.lcg@gmail.com>
2021-03-25 17:59:39 -0300

    challenge-04

---
+++ Desafios/Aula-04/script.js
@@ -0,0 +1,47 @@
+function firstSeason () {
+    document.getElementById("seasons").innerHTML = "1ª Temporada"
+
+    document.getElementById("season-image").innerHTML = '<img src=' + "https://tvpururuca-cdn.nyc3.digitaloceanspaces.com/uploads/2020/09/30092626/peaky-blinders-irmaos-shelby-1.gif" + '>'
+
+    document.getElementById("text-details").innerHTML = '"Birmingham, 1919. Thomas Shelby controls the Peaky Blinders, one most feared criminal of the city organisations, but his ambitions go beyond running the streets. When a crate of guns goes missing, Thomas recognises the opportunity to move up in the world. <br> Wanna know more about it? Click <a href="https://www.imdb.com/title/tt2442560/episodes?season=1&ref_=tt_eps_sn_1" target="_blank">here</a> "'
+}
+
+function secondSeason () {
+    document.getElementById("seasons").innerHTML = "2ª Temporada"
+
+    document.getElementById("season-image").innerHTML = '<img src=' + "https://img.wattpad.com/16edb452541836a25dfac69f9ac3d65ba701689b/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f7279496d6167652f70534e377a5239416c6c425062413d3d2d3537333431333230382e313532646239373438366664363661393435383135363139353035372e676966" + '>'
+
+    document.getElementById("text-details").innerHTML = '"When Birmingham crime boss Tommy Shelby`s beloved Garrison pub is bombed, he finds himself blackmailed into murdering an Irish dissident. <br> Wanna know more about it? Click <a href="https://www.imdb.com/title/tt2442560/episodes?season=2&ref_=tt_eps_sn_2" target="_blank">here</a> "'
+}
+
+function thirdSeason () {
+    document.getElementById("seasons").innerHTML = "3ª Temporada"
+
+    document.getElementById("season-image").innerHTML = '<img src=' + "https://3.bp.blogspot.com/-AAWKDGiB7Ss/WB6iCVNxYOI/AAAAAAAAAu0/KyoFyvvQFqEm2ryGX-VyRe6wvj4tAtrHwCLcB/s1600/peaky%2Bblinders.gif" + '>'
+
+    document.getElementById("text-details").innerHTML = '"It is Thomas Shelby`s long-awaited wedding day. A mysterious visitor imperils the entire Shelby family, and Tommy finds himself pulled into a web of intrigue more lethal than anything he has yet encountered. <br> Wanna know more about it? Click <a href="https://www.imdb.com/title/tt2442560/episodes?season=3&ref_=tt_eps_sn_3" target="_blank">here</a> "'
+}
+
+function fourthSeason () {
+    document.getElementById("seasons").innerHTML = "4ª Temporada"
+
+    document.getElementById("season-image").innerHTML = '<img src=' + "https://img.wattpad.com/b69762edfd334883988ba9de89e772d768b13aa5/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f7279496d6167652f3447634b39374b34686b424353673d3d2d3832353630363738362e313565383136363231346137313564633130363231313436343733352e676966" + '>'
+
+    document.getElementById("text-details").innerHTML = '"It is Christmas and Tommy Shelby receives a letter that makes him realize that he and every member of the family are in danger. He knows that it is time for the family to bury their differences and face the enemy together. <br> Wanna know more about it? Click <a href="https://www.imdb.com/title/tt2442560/episodes?season=4&ref_=tt_eps_sn_4" target="_blank">here</a> "'
+}
+
+function fifthSeason () {
+    document.getElementById("seasons").innerHTML = "5ª Temporada"
+
+    document.getElementById("season-image").innerHTML = '<img src=' + "https://thesaxon.org/wp-content/uploads/2021/01/peaky-blinders-season-5-this-scene-of-rare-violence-with-arthur-shelby-that-you-will-never-see-b2f5f4e.gif" + '>'
+
+    document.getElementById("text-details").innerHTML = '"In the wake of the Wall Street Crash, Tommy faces new dangers from unexpected quarters. <br> Wanna know more about it? Click <a href="https://www.imdb.com/title/tt2442560/episodes?season=5&ref_=tt_eps_sn_5" target="_blank">here</a> "'
+}
+
+function sixthSeason () {
+    document.getElementById("seasons").innerHTML = "6ª Temporada"
+
+    document.getElementById("season-image").innerHTML = '<img src=' + "https://i.pinimg.com/originals/13/89/ee/1389eebe7e05f58f158da51aa33db8f0.gif" + '>'
+
+    document.getElementById("text-details").innerHTML = '"They are filming the sixth season at the moment, but there are no predictions yet. Please, return later. <br> Wanna know more about it? Click <a href="https://www.imdb.com/title/tt2442560/episodes?season=6&ref_=tt_eps_sn_6" target="_blank">here</a> "'
+}
