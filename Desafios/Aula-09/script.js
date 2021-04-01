9b6f3620179aa7f4a5e59330ee4961a4ea12faa0
LeonardoCostaG <leonardocg.lcg@gmail.com>
2021-04-01 20:06:31 -0300

    challange09

---
+++ Desafios/Aula-09/script.js
@@ -0,0 +1,307 @@
+var cartaItadori = {
+    nome: "Yuji Itadori",
+    imagem: "https://i.pinimg.com/originals/8a/6c/7e/8a6c7e0fc2e42e1fb6a230ccad6ed403.jpg",
+    atributos: {
+        'Força Bruta': 90,
+        Jujutsu: 72,
+        'Energia Amaldiçoada': 80
+    }
+}
+
+var cartaMegumi = {
+    nome: "Megumi Fushiguro",
+    imagem: "https://i.ytimg.com/vi/oRyfLcos45k/maxresdefault.jpg",
+    atributos: {
+        'Força Bruta': 82,
+        Jujutsu: 86,
+        'Energia Amaldiçoada': 82
+    }
+}
+
+var cartaNobara = {
+    nome: "Nobara Kugisaki",
+    imagem: "https://ovicio.com.br/wp-content/uploads/2021/03/20210310-nobara-kugisaki.jpg",
+    atributos: {
+        'Força Bruta': 78,
+        Jujutsu: 73,
+        'Energia Amaldiçoada': 74
+    }
+}
+
+var cartaGojo = {
+    nome: "Satoru Gojo",
+    imagem: "https://sm.ign.com/ign_br/screenshot/default/captura-de-tela-277_j48m.png",
+    atributos: {
+        'Força Bruta': 99,
+        Jujutsu: 99,
+        'Energia Amaldiçoada': 99
+    }
+}
+
+var cartaSukuna = {
+    nome: "Sukuna",
+    imagem: "https://sm.ign.com/ign_br/screenshot/default/sukuna-jujutsu-kaisen_gfhh.jpg",
+    atributos: {
+        'Força Bruta': 100,
+        Jujutsu: 100,
+        'Energia Amaldiçoada': 100
+    }
+}
+
+var cartaTodo = {
+    nome: "Aoi Todo",
+    imagem: "https://i.pinimg.com/originals/dc/b6/24/dcb6246b2c749fa88c81634b00a8bb9a.jpg",
+    atributos: {
+        'Força Bruta': 95,
+        Jujutsu: 87,
+        'Energia Amaldiçoada': 85
+    }
+}
+
+var cartaInumaki = {
+    nome: "Toge Inumaki",
+    imagem: "https://i.pinimg.com/originals/b4/33/b9/b433b9bc94ac581531639f51dfe9084e.jpg",
+    atributos: {
+        'Força Bruta': 70,
+        Jujutsu: 87,
+        'Energia Amaldiçoada': 87
+    }
+}
+
+var cartaPanda = {
+    nome: "Panda",
+    imagem: "https://i2.wp.com/loucosporgeek.com.br/wp-content/uploads/2021/01/EmlWY9iU8AA27ib.jpg?resize=708%2C398&ssl=1",
+    atributos: {
+        'Força Bruta': 87,
+        Jujutsu: 80,
+        'Energia Amaldiçoada': 65
+    }
+}
+
+var cartaMaki = {
+    nome: "Maki Zenin",
+    imagem: "https://i.pinimg.com/originals/c0/f1/97/c0f197a20a54b107310150201d4c9b41.jpg",
+    atributos: {
+        'Força Bruta': 85,
+        Jujutsu: 75,
+        'Energia Amaldiçoada': 50
+    }
+}
+
+var cartaUtahime = {
+    nome: "Utahime Iori",
+    imagem: "https://i.pinimg.com/originals/29/bb/c2/29bbc2b9afea9ba97a6259a1bdb330af.jpg",
+    atributos: {
+        'Força Bruta': 89,
+        Jujutsu: 87,
+        'Energia Amaldiçoada': 85
+    }
+}
+
+var cartaMai = {
+    nome: "Mai Zenin",
+    imagem: "https://i.pinimg.com/originals/27/47/6f/27476faae841c2045065f7fb49c37a9f.jpg",
+    atributos: {
+        'Força Bruta': 63,
+        Jujutsu: 72,
+        'Energia Amaldiçoada': 68
+    }
+}
+
+var cartaMiwa = {
+    nome: "Kasumi Miwa",
+    imagem: "https://i.pinimg.com/originals/90/d6/c2/90d6c26a7ebd1e3d9bb1b5a56cc9b654.jpg",
+    atributos: {
+        'Força Bruta': 72,
+        Jujutsu: 68,
+        'Energia Amaldiçoada': 61
+    }
+}
+
+var cartaKamo = {
+    nome: "Noritoshi Kamo",
+    imagem: "https://i.ytimg.com/vi/n5DnbnFHuco/maxresdefault.jpg",
+    atributos: {
+        'Força Bruta': 83,
+        Jujutsu: 85,
+        'Energia Amaldiçoada': 86
+    }
+}
+
+var cartaNanami = {
+    nome: "Kento Nanami",
+    imagem: "https://media.comicbook.com/2021/02/jujutsu-kaisen-nanami-kento-anime-1255054-1280x0.jpeg",
+    atributos: {
+        'Força Bruta': 92,
+        Jujutsu: 85,
+        'Energia Amaldiçoada': 87
+    }
+}
+
+var cartaMahito = {
+    nome: "Mahito",
+    imagem: "https://i.pinimg.com/originals/03/1a/54/031a5485450b9093554b64b4af1f0cdf.jpg",
+    atributos: {
+        'Força Bruta': 93,
+        Jujutsu: 0,
+        'Energia Amaldiçoada': 92
+    }
+}
+
+var cartaJogo = {
+    nome: "Jogo",
+    imagem: "https://i.pinimg.com/originals/bb/14/2e/bb142e1d18dcf96284f1a082182156f3.jpg",
+    atributos: {
+        'Força Bruta': 83,
+        Jujutsu: 0,
+        'Energia Amaldiçoada': 79
+    }
+}
+
+var cartaHanami = {
+    nome: "Hanami",
+    imagem: "https://otakukart.com/wp-content/uploads/2021/02/jujutsu-kaisem-1024x576.jpg",
+    atributos: {
+        'Força Bruta': 89,
+        Jujutsu: 0,
+        'Energia Amaldiçoada': 75
+    }
+}
+
+var cartaNishimiya = {
+    nome: "Momo Nishimiya",
+    imagem: "https://i.pinimg.com/originals/6c/73/ba/6c73ba22db6573bdc46f408bb914d50c.jpg",
+    atributos: {
+        'Força Bruta': 73,
+        Jujutsu: 79,
+        'Energia Amaldiçoada': 76
+    }
+}
+
+var cartaMaquina
+var cartaJogador
+var cartas = [cartaNishimiya, cartaHanami, cartaJogo, cartaMahito, cartaNanami, cartaKamo, cartaMiwa, cartaMai, cartaUtahime, cartaMaki, cartaPanda, cartaInumaki, cartaTodo, cartaSukuna, cartaGojo, cartaNobara, cartaMegumi, cartaItadori]
+
+var pontosJogador = 0
+var pontosMaquina = 0
+
+atualizaPlacar()
+atualizaQuantidadeDeCartas()
+
+function atualizaQuantidadeDeCartas() {
+  var divQuantidadeCartas = document.getElementById('quantidade-cartas')
+  var html = "Quantidade de rodadas restantes: " + cartas.length/2
+  
+  divQuantidadeCartas.innerHTML = html
+}
+
+function atualizaPlacar() {
+  var divPlacar = document.getElementById("placar")
+  var html = "Jogador " + pontosJogador + "x" + pontosMaquina + " Máquina"
+  
+  divPlacar.innerHTML = html
+}
+
+function sortearCarta() {
+    var numeroCartaMaquina = parseInt(Math.random() * cartas.length)
+    cartaMaquina = cartas[numeroCartaMaquina]
+    cartas.splice(numeroCartaMaquina, 1)
+
+    var numeroCartaJogador = parseInt(Math.random() * cartas.length)
+    cartaJogador = cartas[numeroCartaJogador]
+    cartas.splice(numeroCartaJogador, 1)
+
+    document.getElementById('btnSortear').disabled = true
+    document.getElementById('btnJogar').disabled = false
+
+    exibeCartaJogador()
+}
+
+function exibeCartaJogador() {
+    var divCartaJogador = document.getElementById("carta-jogador")
+    var moldura = '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
+    divCartaJogador.style.backgroundImage = `url(${cartaJogador.imagem})`
+    var nome = `<p class="carta-subtitle">${cartaJogador.nome}</p>`
+    var opcoesTexto = ""
+
+    for (var atributo in cartaJogador.atributos) {
+        opcoesTexto += "<input type='radio' name='atributo' value='" + atributo + "'>" + atributo + " - " + cartaJogador.atributos[atributo] + "<br>"
+    }
+
+    var html = "<div id='opcoes' class='carta-status'>"
+
+    divCartaJogador.innerHTML = moldura + nome + html + opcoesTexto + '</div>'
+}
+
+function obtemAtributoSelecionado() {
+    var radioAtributo = document.getElementsByName('atributo')
+    for (var i = 0; i < radioAtributo.length; i++) {
+        if (radioAtributo[i].checked) {
+            return radioAtributo[i].value
+        }
+    }
+}
+
+function jogar() {
+    var divResultado = document.getElementById("resultado")
+    var atributoSelecionado = obtemAtributoSelecionado()
+
+    if (cartaJogador.atributos[atributoSelecionado] > cartaMaquina.atributos[atributoSelecionado]) {
+        htmlResultado = '<p class="resultado-final">Venceu</p>'
+      pontosJogador++
+    } else if (cartaJogador.atributos[atributoSelecionado] < cartaMaquina.atributos[atributoSelecionado]) {
+        htmlResultado = '<p class="resultado-final">Perdeu</p>'
+      pontosMaquina++
+    } else {
+        htmlResultado = '<p class="resultado-final">Empatou</p>'
+    }
+  
+    if (cartas.length == 0) {
+      alert("Fim de jogo")
+      if (pontosJogador > pontosMaquina) {
+        htmlResultado = '<p class="resultado-final">Você exorcizou o adversário!</p>'
+      } else if (pontosMaquina > pontosJogador) {
+        htmlResultado = '<p class="resultado-final">Você foi esmagado!</p>'
+      } else {
+        htmlResultado = '<p class="resultado-final">Vocês empataram!</p>'
+      }
+    } else {
+      document.getElementById('btnProximaRodada').disabled = false
+    }
+
+    divResultado.innerHTML = htmlResultado
+    document.getElementById('btnJogar').disabled = true
+    atualizaPlacar()
+    exibeCartaMaquina()
+    atualizaQuantidadeDeCartas()
+}
+
+function exibeCartaMaquina() {
+    var divCartaMaquina = document.getElementById("carta-maquina")
+    var moldura = '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;">';
+    divCartaMaquina.style.backgroundImage = `url(${cartaMaquina.imagem})`
+    var nome = `<p class="carta-subtitle">${cartaMaquina.nome}</p>`
+    var opcoesTexto = ""
+
+    for (var atributo in cartaMaquina.atributos) {
+        console.log(atributo)
+        opcoesTexto += "<p type='text' name='atributo' value='" + atributo + "'>" + atributo + " " + cartaMaquina.atributos[atributo] + "<br>"
+    }
+
+    var html = "<div id='opcoes' class='carta-status --spacing'>"
+
+    divCartaMaquina.innerHTML = moldura + nome + html + opcoesTexto + '</div>'
+}
+
+function proximaRodada() {
+  var divCartas = document.getElementById('cartas')
+  
+  divCartas.innerHTML = `<div id="carta-jogador" class="carta"></div> <div id="carta-maquina" class="carta"></div>`
+  
+  document.getElementById('btnSortear').disabled = false
+  document.getElementById('btnJogar').disabled = true
+  document.getElementById('btnProximaRodada').disabled = true
+  
+  var divResultado = document.getElementById('resultado')
+  divResultado.innerHTML = ""
+}
